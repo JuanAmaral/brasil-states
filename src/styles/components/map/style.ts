63320bd6cbbeac957ebe047aab41e9b26a62f0f9
JuanAmaral <lgdfjktg@gmail.com>
2025-01-09 23:36:21 -0300

    Update style.ts

--- src/styles/components/map/style.ts
+++ src/styles/components/map/style.ts
@@ -1,7 +1,9 @@
 import styled from "styled-components";
+interface ContainerMapProps {
+    children?: React.ReactNode;
+  }
 
-
-export const ContainerMap = styled.div`
+export const ContainerMap = styled.div<ContainerMapProps>`
     path:hover{
         fill:#439750;
         cursor:pointer;
